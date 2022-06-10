1ff3ac61335a8a2dc55c18abde5197fbb49f24ba
MarshallChiang <ambrosia8286@gmail.com>
2022-06-10 16:24:55 +0800

    feat(logistics): add result validation to statusMap

--- packages/logistics-adapter-tcat/__tests__/delivery-adapter-tcat.spec.ts
+++ packages/logistics-adapter-tcat/__tests__/delivery-adapter-tcat.spec.ts
@@ -4,6 +4,8 @@ describe('delivery-adapter-tcat', () => {
     const logistics = new TCatLogisticsService(TCatLogistics)
 
     it('should trace logistics', async () => {
-        const trace = await logistics.trace('800978442950')
+        const trace = await logistics.trace(['903404283301', '800978442950', '135006120322', 'AAAA'])
+
+        trace.map(t => console.log(t))
     });
 });
