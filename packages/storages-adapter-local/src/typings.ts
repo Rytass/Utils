400573d26ef2f10f16d012591276755af89f35f0
Yuuko <aznrogerazn@gmail.com>
2023-05-30 14:45:20 +0800

    feat: storage helper commands: total

--- packages/storages-adapter-local/src/typings.ts
+++ packages/storages-adapter-local/src/typings.ts
@@ -9,6 +9,7 @@ export interface StorageLocalOptions extends StorageOptions {
 export enum StorageLocalHelperCommands {
   USED = 'du -sm __DIR__ | awk \'{ print $1 }\'',
   FREE = 'df -m __DIR__ | awk \'$3 ~ /[0-9]+/ { print $4 }\'',
+  TOTAL = 'df -m __DIR__ | awk \'$3 ~ /[0-9]+/ { print $2 }\'',
 }
 
 // @dev: in 1M blocks
