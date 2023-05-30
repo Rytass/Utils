ca46aa50a4bd2b2cc0911419ce5839a0f23eb380
Yuuko <aznrogerazn@gmail.com>
2023-05-30 14:22:33 +0800

    feat: storage helper commands for *nix systems

--- packages/storages-adapter-local/src/typings.ts
+++ packages/storages-adapter-local/src/typings.ts
@@ -4,3 +4,9 @@ export interface StorageLocalOptions extends StorageOptions {
   directory: string;
   autoMkdir?: boolean;
 }
+
+// @dev: for *NIX systems
+export enum StorageLocalHelperCommands {
+  USED = 'du -sm . | awk \'{ print $1 }\'',
+  FREE = 'df -m . | awk \'$3 ~ /[0-9]+/ { print $4 }\'',
+}
