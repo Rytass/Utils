19f3a7d05af93407b5c5a70c0d25f70522f8068b
Chia Yu Pai <fantasyatelier@gmail.com>
2024-07-23 18:59:35 +0800

    chore: change to use nx

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  '*.@(js|jsx|ts|tsx)': ['eslint --ext .js,.jsx,.ts,.tsx --fix'],
+  'packages/**/*.{ts,tsx}': files => `nx affected --target=typecheck --files=${files.join(',')}`,
+  'packages/**/*.{js,jsx,ts,tsx,json,css,scss,json}': [
+    files => `nx affected:lint --fix --files=${files.join(',')}`,
+    files => `nx format:write --files=${files.join(',')}`,
+  ],
 };
