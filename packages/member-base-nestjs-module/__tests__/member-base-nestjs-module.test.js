5d4e9e9b8e648520da52e596d23f389d35e0036d
Chia Yu Pai <fantasyatelier@gmail.com>
2024-07-23 20:08:37 +0800

    chore(member-base-nestjs-module): remove invalid spec

--- packages/member-base-nestjs-module/__tests__/member-base-nestjs-module.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const memberBaseNestjsModule = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(memberBaseNestjsModule(), 'Hello from memberBaseNestjsModule');
-console.info('memberBaseNestjsModule tests passed');
