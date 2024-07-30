7076b528ac9eaea097e22f25c214c3da6acb20b2
Chia Yu Pai <fantasyatelier@gmail.com>
2024-07-30 16:25:33 +0800

    feat(cms-base-nestjs-module): export category service

--- packages/cms-base-nestjs-module/src/cms-base.module.ts
+++ packages/cms-base-nestjs-module/src/cms-base.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CMSBaseModelsModule } from './models/models.module';
 import { ArticleBaseService } from './services/article-base.service';
+import { CategoryBaseService } from './services/category-base.service';
 
 @Module({
   imports: [CMSBaseModelsModule],
-  providers: [ArticleBaseService],
-  exports: [CMSBaseModelsModule, ArticleBaseService],
+  providers: [ArticleBaseService, CategoryBaseService],
+  exports: [CMSBaseModelsModule, ArticleBaseService, CategoryBaseService],
 })
 export class CMSBaseModule {}
