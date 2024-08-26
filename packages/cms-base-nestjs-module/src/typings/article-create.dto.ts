448550f7fffba0b9c1666fa767e9f598c8d6efcc
Chia Yu Pai <fantasyatelier@gmail.com>
2024-08-26 11:34:28 +0800

    fix(cms-base-nestjs-module): typing DeepPartial -> Partial

--- packages/cms-base-nestjs-module/src/typings/article-create.dto.ts
+++ packages/cms-base-nestjs-module/src/typings/article-create.dto.ts
@@ -1,30 +1,30 @@
-import { QuadratsElement } from '@quadrats/core';
 import { Language } from './language';
 import {
   BaseArticleEntity,
   BaseArticleVersionContentEntity,
   BaseArticleVersionEntity,
 } from '../models';
-import { DeepPartial } from 'typeorm';
 
 type BaseArticleCreateDto<
   A extends BaseArticleEntity = BaseArticleEntity,
   AV extends BaseArticleVersionEntity = BaseArticleVersionEntity,
-> = Omit<A, 'versions' | 'categories'> &
-  Omit<
-    AV,
-    | 'articleId'
-    | 'createdAt'
-    | 'deletedAt'
-    | 'article'
-    | 'multiLanguageContents'
-  > & {
-    categoryIds?: string[];
-  };
+> = Partial<
+  Omit<A, 'versions' | 'categories'> &
+    Omit<
+      AV,
+      | 'articleId'
+      | 'createdAt'
+      | 'deletedAt'
+      | 'article'
+      | 'multiLanguageContents'
+    > & {
+      categoryIds?: string[];
+    }
+>;
 
 export type SingleVersionContentCreateDto<
   AVC extends BaseArticleVersionContentEntity = BaseArticleVersionContentEntity,
-> = Omit<AVC, 'articleId' | 'version' | 'language' | 'articleVersion'>;
+> = Partial<Omit<AVC, 'articleId' | 'version' | 'language' | 'articleVersion'>>;
 
 export type SingleArticleCreateDto<
   A extends BaseArticleEntity = BaseArticleEntity,
