cefa781ff3d00401618e13aca9de34e0d19d257a
Chia Yu Pai <fantasyatelier@gmail.com>
2025-02-19 16:49:01 +0800

    feat(member-base-nestjs-module): export MemberOAuthRecordEntity

--- packages/member-base-nestjs-module/src/models/index.ts
+++ packages/member-base-nestjs-module/src/models/index.ts
@@ -1,3 +1,4 @@
 export { BaseMemberEntity } from './base-member.entity';
 export * from './member-login-log.entity';
 export * from './member-password-history.entity';
+export * from './member-oauth-record.entity';
