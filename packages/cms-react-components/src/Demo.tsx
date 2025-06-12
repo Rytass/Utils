46ae7db75ecad4135a127b2bd110a56e21ece3ec
TingTing <ting@rytass.com>
2025-06-12 15:32:18 +0800

    refactor(packages/cms-react-components): rename package

--- packages/cms-react-components/src/Demo.tsx
+++ packages/cms-react-components/src/Demo.tsx
@@ -4,11 +4,11 @@ import { Button, Typography } from '@mezzanine-ui/react';
 const Demo = () => {
   return (
     <div>
-      Hi
       <Button type="button" variant="contained">
         button
       </Button>
       <Typography variant="h1">h1</Typography>
+      <Typography variant="h2">h2</Typography>
     </div>
   );
 };
