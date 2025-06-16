d884520e7c9e712c818039970d4dba7abda00ad0
TingTing <ting@rytass.com>
2025-06-16 15:41:48 +0800

    feat(packages/cms-react-components): defined permissions and article actions

--- packages/cms-react-components/src/constants.ts
+++ packages/cms-react-components/src/constants.ts
@@ -1,15 +1,77 @@
-import { ArticlesPermissions } from './typings';
+import {
+  ArticleTableActions,
+  ArticlesPermissions,
+  ArticleTableActionsType,
+  ArticleStage,
+} from './typings';
 
 export const defaultAdminRolePermissions = [
-  ArticlesPermissions.CreateUpdateArticle,
-  ArticlesPermissions.DeleteArticle,
+  ArticlesPermissions.CreateArticle,
   ArticlesPermissions.ApproveRejectArticle,
-  ArticlesPermissions.ReleaseUnreleaseArticle,
+  // Draft
+  ArticlesPermissions.UpdateArticleInDraft,
+  ArticlesPermissions.ReleaseArticleInDraft,
+  ArticlesPermissions.DeleteArticleInDraft,
+  // Reviewing
+  ArticlesPermissions.UpdateArticleInReviewing,
+  ArticlesPermissions.DeleteArticleInReviewing,
+  // Verified
+  ArticlesPermissions.UpdateArticleInVerified,
+  ArticlesPermissions.ReleaseArticleInVerified,
+  ArticlesPermissions.DeleteArticleInVerified,
+  // Scheduled
+  ArticlesPermissions.UpdateArticleInScheduled,
+  ArticlesPermissions.ReleaseArticleInScheduled,
+  ArticlesPermissions.UnreleaseArticleInScheduled,
+  // Released
+  ArticlesPermissions.UpdateArticleInReleased,
+  ArticlesPermissions.ReleaseArticleInReleased,
+  ArticlesPermissions.DeleteArticleInReleased,
 ];
 
 export const defaultGeneralRolePermissions = [
-  ArticlesPermissions.CreateUpdateArticle,
-  ArticlesPermissions.DeleteArticle,
+  ArticlesPermissions.CreateArticle,
   ArticlesPermissions.SubmitPutBackArticle,
-  ArticlesPermissions.ReleaseUnreleaseArticle,
+  // Draft
+  ArticlesPermissions.UpdateArticleInDraft,
+  ArticlesPermissions.DeleteArticleInDraft,
+  // Reviewing
+  ArticlesPermissions.UpdateArticleInReviewing,
+  // Verified
+  ArticlesPermissions.ReleaseArticleInVerified,
+  // Scheduled
+  ArticlesPermissions.ReleaseArticleInScheduled,
+  ArticlesPermissions.UnreleaseArticleInScheduled,
+  // Released
+  ArticlesPermissions.UpdateArticleInReleased,
 ];
+
+export const defaultTableActions: ArticleTableActionsType = {
+  [ArticleStage.DRAFT]: [
+    ArticleTableActions.Update,
+    ArticleTableActions.Submit,
+    ArticleTableActions.Release,
+    ArticleTableActions.Delete,
+  ],
+  [ArticleStage.REVIEWING]: [
+    ArticleTableActions.Update,
+    ArticleTableActions.Review,
+    ArticleTableActions.Delete,
+    ArticleTableActions.PutBack,
+  ],
+  [ArticleStage.VERIFIED]: [
+    ArticleTableActions.View,
+    ArticleTableActions.Update,
+    ArticleTableActions.Release,
+    ArticleTableActions.Delete,
+  ],
+  [ArticleStage.SCHEDULED]: [
+    ArticleTableActions.View,
+    ArticleTableActions.Update,
+    ArticleTableActions.Unrelease,
+  ],
+  [ArticleStage.RELEASED]: [
+    ArticleTableActions.Update,
+    ArticleTableActions.Delete,
+  ],
+};
--- packages/cms-react-components/src/typings.ts
+++ packages/cms-react-components/src/typings.ts
@@ -6,10 +6,72 @@ export enum ArticleStage {
   RELEASED = 'RELEASED',
 }
 
+export enum ArticleTableActions {
+  View = 'View',
+  Update = 'Update',
+  Delete = 'Delete',
+  Submit = 'Submit',
+  PutBack = 'PutBack',
+  Review = 'Review',
+  Release = 'Release',
+  Unrelease = 'Unrelease',
+}
+
+export interface ArticleTableActionsType {
+  [ArticleStage.DRAFT]: (
+    | ArticleTableActions.Update
+    | ArticleTableActions.Submit
+    | ArticleTableActions.Release
+    | ArticleTableActions.Delete
+  )[];
+  [ArticleStage.REVIEWING]: (
+    | ArticleTableActions.Update
+    | ArticleTableActions.Review
+    | ArticleTableActions.Delete
+    | ArticleTableActions.PutBack
+  )[];
+  [ArticleStage.VERIFIED]: (
+    | ArticleTableActions.View
+    | ArticleTableActions.Update
+    | ArticleTableActions.Release
+    | ArticleTableActions.Delete
+  )[];
+  [ArticleStage.SCHEDULED]: (
+    | ArticleTableActions.View
+    | ArticleTableActions.Update
+    | ArticleTableActions.Unrelease
+  )[];
+  [ArticleStage.RELEASED]: (
+    | ArticleTableActions.Update
+    | ArticleTableActions.Delete
+  )[];
+}
+
 export enum ArticlesPermissions {
-  CreateUpdateArticle = 'CreateUpdateArticle',
-  DeleteArticle = 'DeleteArticle',
+  CreateArticle = 'CreateArticle',
   SubmitPutBackArticle = 'SubmitPutBackArticle',
   ApproveRejectArticle = 'ApproveRejectArticle',
-  ReleaseUnreleaseArticle = 'ReleaseUnreleaseArticle',
+  // Draft
+  UpdateArticleInDraft = 'UpdateArticleInDraft',
+  ReleaseArticleInDraft = 'ReleaseArticleInDraft',
+  DeleteArticleInDraft = 'DeleteArticleInDraft',
+
+  // Reviewing
+  UpdateArticleInReviewing = 'UpdateArticleInReviewing',
+  DeleteArticleInReviewing = 'DeleteArticleInReviewing',
+
+  // Verified
+  UpdateArticleInVerified = 'UpdateArticleInVerified',
+  ReleaseArticleInVerified = 'ReleaseArticleInVerified',
+  DeleteArticleInVerified = 'DeleteArticleInVerified',
+
+  // Scheduled
+  UpdateArticleInScheduled = 'UpdateArticleInScheduled',
+  ReleaseArticleInScheduled = 'ReleaseArticleInScheduled',
+  UnreleaseArticleInScheduled = 'UnreleaseArticleInScheduled',
+
+  // Released
+  UpdateArticleInReleased = 'UpdateArticleInReleased',
+  ReleaseArticleInReleased = 'ReleaseArticleInReleased',
+  DeleteArticleInReleased = 'DeleteArticleInReleased',
 }
