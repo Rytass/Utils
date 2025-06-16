927544034b1b14d9b3803d315540213de9cd27ea
TingTing <ting@rytass.com>
2025-06-16 17:28:16 +0800

    feat(packages/cms-react-components): rename component

---
+++ packages/cms-react-components/src/components/StandardCMSFormActions/index.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import classes from './index.module.scss';
+
+const StandardCMSFormActions = (): ReactElement => {
+  return <div className={classes.root}>StandardCMSFormActions</div>;
+};
+
+export { StandardCMSFormActions };
