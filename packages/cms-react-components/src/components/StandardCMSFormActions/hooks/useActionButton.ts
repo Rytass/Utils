61b8297686d7583e16ebb77664a00b45293144b5
TingTing <ting@rytass.com>
2025-06-18 18:37:46 +0800

    feat(packages/cms-react-components): give props to hooks

--- packages/cms-react-components/src/components/StandardCMSFormActions/hooks/useActionButton.ts
+++ packages/cms-react-components/src/components/StandardCMSFormActions/hooks/useActionButton.ts
@@ -1,4 +1,18 @@
-export function useActionButton(): {
+import { FieldValues } from 'react-hook-form';
+import { StandardCMSFormActionsEventsProps } from '../typings';
+import { ArticleStage, ArticlesPermissions } from '../../../typings';
+
+export function useActionButton<T extends FieldValues>({
+  values,
+  currentStage,
+  userPermissions,
+  actionsEvents,
+}: {
+  values: T;
+  currentStage: ArticleStage;
+  userPermissions: ArticlesPermissions[];
+  actionsEvents: StandardCMSFormActionsEventsProps<T>;
+}): {
   text: string;
   onAction?: () => Promise<void>;
 } {
--- packages/cms-react-components/src/components/StandardCMSFormActions/hooks/useSubmitButton.ts
+++ packages/cms-react-components/src/components/StandardCMSFormActions/hooks/useSubmitButton.ts
@@ -1,4 +1,18 @@
-export function useSubmitButton(): {
+import { FieldValues } from 'react-hook-form';
+import { StandardCMSFormActionsEventsProps } from '../typings';
+import { ArticleStage, ArticlesPermissions } from '../../../typings';
+
+export function useSubmitButton<T extends FieldValues>({
+  values,
+  currentStage,
+  userPermissions,
+  actionsEvents,
+}: {
+  values: T;
+  currentStage: ArticleStage;
+  userPermissions: ArticlesPermissions[];
+  actionsEvents: StandardCMSFormActionsEventsProps<T>;
+}): {
   text: string;
   onSubmit?: () => Promise<void>;
 } {
