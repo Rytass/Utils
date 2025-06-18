8ed0ccd22be6cec916d525a2e4f56c7d1d35eac4
TingTing <ting@rytass.com>
2025-06-18 18:23:58 +0800

    feat(packages/cms-react-components): use react hook to maintain button behavior

--- packages/cms-react-components/src/components/StandardCMSFormActions/FormActionsBody.tsx
+++ packages/cms-react-components/src/components/StandardCMSFormActions/FormActionsBody.tsx
@@ -1,22 +1,24 @@
 import React, { ReactNode, useCallback } from 'react';
 import { FieldValues, FormProvider } from 'react-hook-form';
 import FormActionsFooter from './FormActionsFooter';
+import { useSubmitButton } from './hooks/useSubmitButton';
 import { StandardCMSFormActionsProps } from './typings';
 
 const FormActionsBody = <T extends FieldValues>(
   props: StandardCMSFormActionsProps<T>,
 ): ReactNode => {
   const { className, methods, children, onSubmit: onSubmitProps } = props;
+  const submitButton = useSubmitButton();
 
   const onSubmit = useCallback(
     async (value: T) => {
       if (onSubmitProps) {
         await onSubmitProps(value);
       } else {
-        console.log('value', value);
+        await submitButton.onSubmit?.();
       }
     },
-    [onSubmitProps],
+    [onSubmitProps, submitButton],
   );
 
   return (
