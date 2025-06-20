e9480cbe80a83fc64e1051610944c37a81b64881
TingTing <ting@rytass.com>
2025-06-20 12:53:17 +0800

    feat(packages/cms-react-components): logs modal block layout

--- packages/cms-react-components/src/components/cms-modals/LogsModal/index.tsx
+++ packages/cms-react-components/src/components/cms-modals/LogsModal/index.tsx
@@ -3,6 +3,8 @@ import {
   ModalHeader,
   ModalBody as MznModalBody,
   ModalActions,
+  Typography,
+  Button,
 } from '@mezzanine-ui/react';
 import { useModal } from '../../modal/useModal';
 import classes from './index.module.scss';
@@ -15,7 +17,48 @@ const LogsModal = (): ReactNode => {
   return (
     <>
       <ModalHeader showSeverityIcon={false}>版本資訊</ModalHeader>
-      <MznModalBody className={classes.modalBody}></MznModalBody>
+      <MznModalBody className={classes.modalBody}>
+        <div className={classes.block}>
+          <div className={classes.timeLineWrapper}>
+            <div className={classes.topLine} />
+            <div className={classes.dot} />
+            <div className={classes.bottomLine} />
+          </div>
+          <div className={classes.contentWrapper}>
+            <div className={classes.stageWrapper}>
+              <Typography variant="h5" color="text-primary">
+                待審核
+              </Typography>
+              <Button
+                type="button"
+                variant="text"
+                color="secondary"
+                size="small"
+              >
+                Ver. 4
+              </Button>
+            </div>
+            <div className={classes.list}>
+              <div className={classes.option}>
+                <Typography variant="h6" color="text-secondary">
+                  送審時間
+                </Typography>
+                <Typography variant="body2" color="text-primary">
+                  2025-05-21．18:00
+                </Typography>
+              </div>
+              <div className={classes.option}>
+                <Typography variant="h6" color="text-secondary">
+                  送審人員
+                </Typography>
+                <Typography variant="body2" color="text-primary">
+                  staff@email.com
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MznModalBody>
       <ModalActions
         cancelText="取消"
         confirmText="關閉"
