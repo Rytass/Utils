d12505f0eb24a32cdecec3739577cdd597c8d989
TingTing <ting@rytass.com>
2025-06-20 12:33:31 +0800

    feat(packages/cms-react-components): create logs modal component

--- packages/cms-react-components/src/components/StandardCMSTable/Table.tsx
+++ packages/cms-react-components/src/components/StandardCMSTable/Table.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement, useMemo, useCallback } from 'react';
 import { compact } from 'lodash';
 import { Table as MznTable, IconButton, Icon } from '@mezzanine-ui/react';
 import { TableColumn, TableDataSourceWithID } from '@mezzanine-ui/core/table';
 import { VersionLog } from '../../icons/version-log';
+import { useModal } from '../modal/useModal';
+import { LogsModal } from '../cms-modals/LogsModal';
 import { useTableActions } from './hooks/useTableActions';
 import { StandardCMSTableProps } from './typings';
 
@@ -30,12 +32,23 @@ const Table = <T extends TableDataSourceWithID>({
   actions,
   customizedActions = [],
 }: StandardCMSTableProps<T>): ReactElement => {
+  const { openModal } = useModal();
   const tableActions = useTableActions<T>({
     currentStage,
     userPermissions,
     actionsEvents,
     actions,
   });
+
+  const onClickVersionIcon = useCallback(
+    (source: T) => () => {
+      openModal({
+        severity: 'info',
+        children: <LogsModal />,
+      });
+    },
+    [openModal],
+  );
 
   const columns = useMemo(
     (): TableColumn<T>[] =>
@@ -44,7 +57,11 @@ const Table = <T extends TableDataSourceWithID>({
           title: '',
           width: 60,
           render: (source) => (
-            <IconButton type="button" size="small">
+            <IconButton
+              type="button"
+              size="small"
+              onClick={onClickVersionIcon(source)}
+            >
               <Icon icon={VersionLog} size={16} />
             </IconButton>
           ),
@@ -53,7 +70,13 @@ const Table = <T extends TableDataSourceWithID>({
         ...tableActions,
         ...customizedActions,
       ]),
-    [withVersionLogs, columnsProps, tableActions, customizedActions],
+    [
+      withVersionLogs,
+      columnsProps,
+      tableActions,
+      customizedActions,
+      onClickVersionIcon,
+    ],
   );
 
   const baseTableProps = useMemo(
--- packages/cms-react-components/src/components/dialog/useDialog.ts
+++ packages/cms-react-components/src/components/dialog/useDialog.ts
@@ -14,35 +14,8 @@ export const useDialog = (): DialogHookValue => {
       });
     });
 
-  const openCancelConfirmDialog = async (
-    onCancel: () => void,
-    config?: CancelConfirmDialogConfig,
-  ) => {
-    const isConfirm = await openDialog({
-      severity: config?.severity ?? 'warning',
-      size: config?.size ?? 'small',
-      title: config?.title ?? '確認離開編輯？',
-      children:
-        config?.children ??
-        '編輯將不被保存，如果需要保存目前文章編輯進度，請選擇「新增草稿版本」。',
-      cancelText: config?.cancelText ?? '取消',
-      cancelButtonProps: {
-        danger: false,
-      },
-      confirmText: config?.confirmText ?? '離開編輯',
-      confirmButtonProps: {
-        danger: config?.isConfirmDanger ?? false,
-      },
-    });
-
-    if (isConfirm) {
-      onCancel();
-    }
-  };
-
   return {
     openDialog,
-    openCancelConfirmDialog,
     closeDialog,
   };
 };
