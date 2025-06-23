09bc2c2a57cca2d9175ce39eb297e5bb1ee46c79
Chia Yu Pai <fantasyatelier@gmail.com>
2025-06-23 16:41:00 +0800

    fix(cms-base-nestjs-graphql-module): support multi language option

--- packages/cms-base-nestjs-graphql-module/src/resolvers/article.resolver.ts
+++ packages/cms-base-nestjs-graphql-module/src/resolvers/article.resolver.ts
@@ -5,17 +5,21 @@ import { UserDto } from '../dto/user.dto';
 import { IsPublic } from '@rytass/member-base-nestjs-module';
 import {
   DEFAULT_LANGUAGE,
+  MULTIPLE_LANGUAGE_MODE,
   type ArticleBaseDto,
 } from '@rytass/cms-base-nestjs-module';
 import { CategoryDto } from '../dto/category.dto';
 import { ArticleDataLoader } from '../dataloaders/article.dataloader';
 import { Language } from '../decorators/language.decorator';
+import { Inject } from '@nestjs/common';
 
 @Resolver(() => ArticleDto)
 export class ArticleResolver {
   constructor(
     private readonly memberDataloader: MemberDataLoader,
     private readonly articleDataloader: ArticleDataLoader,
+    @Inject(MULTIPLE_LANGUAGE_MODE)
+    private readonly multiLanguage: boolean,
   ) {}
 
   @ResolveField(() => UserDto, { nullable: true })
@@ -34,7 +38,7 @@ export class ArticleResolver {
   ): Promise<CategoryDto[]> {
     return this.articleDataloader.categoriesLoader.load({
       articleId: article.id,
-      language,
+      language: this.multiLanguage ? language : DEFAULT_LANGUAGE,
     });
   }
 }
