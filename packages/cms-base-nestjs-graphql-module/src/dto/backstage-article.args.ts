14fa2fda9cfefbebab9db0b55766f37092402fbb
Luca <lucaluca.tw@gmail.com>
2025-07-14 17:10:42 +0800

    feat(cms-base-nestjs-graphql-module): add sorter field to BackstageArticleArgs for article sorting

--- packages/cms-base-nestjs-graphql-module/src/dto/backstage-article.args.ts
+++ packages/cms-base-nestjs-graphql-module/src/dto/backstage-article.args.ts
@@ -1,13 +1,20 @@
 import { ArgsType, Field, registerEnumType } from '@nestjs/graphql';
-import { ArticleStage } from '@rytass/cms-base-nestjs-module';
+import { ArticleSorter, ArticleStage } from '@rytass/cms-base-nestjs-module';
 import { ArticlesArgs } from './articles.args';
 
 @ArgsType()
 export class BackstageArticleArgs extends ArticlesArgs {
   @Field(() => ArticleStage)
   stage: ArticleStage;
+
+  @Field(() => ArticleSorter, { nullable: true })
+  sorter?: ArticleSorter | null;
 }
 
 registerEnumType(ArticleStage, {
   name: 'ArticleStage',
+});
+
+registerEnumType(ArticleSorter, {
+  name: 'ArticleSorter',
 });
