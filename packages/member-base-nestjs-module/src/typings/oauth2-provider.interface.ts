9da564c80ad07e1ceadc4a6d43ea0adaf654d3a5
Chia Yu Pai <fantasyatelier@gmail.com>
2025-09-18 01:38:08 +0800

    refactor(member-base-nestjs-module): streamline OAuth2 provider interfaces and channels

--- packages/member-base-nestjs-module/src/typings/oauth2-provider.interface.ts
+++ packages/member-base-nestjs-module/src/typings/oauth2-provider.interface.ts
@@ -1,5 +1,7 @@
+export type OAuthChannel = 'google' | 'facebook' | (string & {});
+
 export interface BaseOAuth2Provider {
-  channel: 'google' | 'facebook' | string;
+  channel: OAuthChannel;
   clientId: string;
   clientSecret: string;
   redirectUri: string;
@@ -8,32 +10,30 @@ export interface BaseOAuth2Provider {
 
 export interface GoogleOAuth2Provider extends BaseOAuth2Provider {
   channel: 'google';
-  clientId: string;
-  clientSecret: string;
-  redirectUri: string;
   scope?: string[];
-  getState?: () => string | Promise<string>;
 }
 
 export interface FacebookOAuth2Provider extends BaseOAuth2Provider {
   channel: 'facebook';
-  clientId: string;
-  clientSecret: string;
-  redirectUri: string;
   scope?: string[];
-  getState?: () => string | Promise<string>;
 }
 
 export interface CustomOAuth2Provider extends BaseOAuth2Provider {
   channel: string;
-  clientId: string;
-  clientSecret: string;
-  redirectUri: string;
   scope: string[];
   requestUrl: string;
   getAccessTokenFromCode: (code: string) => Promise<string>;
   getAccountFromAccessToken: (accessToken: string) => Promise<string>;
-  getState?: () => string | Promise<string>;
 }
 
 export type OAuth2Provider = GoogleOAuth2Provider | FacebookOAuth2Provider | CustomOAuth2Provider;
+
+// Type guards for OAuth2 providers
+export const isGoogleProvider = (provider: OAuth2Provider): provider is GoogleOAuth2Provider =>
+  provider.channel === 'google';
+
+export const isFacebookProvider = (provider: OAuth2Provider): provider is FacebookOAuth2Provider =>
+  provider.channel === 'facebook';
+
+export const isCustomProvider = (provider: OAuth2Provider): provider is CustomOAuth2Provider =>
+  provider.channel !== 'google' && provider.channel !== 'facebook';
