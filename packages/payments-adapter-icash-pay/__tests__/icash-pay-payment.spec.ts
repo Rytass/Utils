f2edb589b9d8f0700934b826bd9726ef878eb5f3
geoffreyfarel <geoffreyfarrel67@gmail.com>
2025-09-30 17:29:49 +0800

    chore: testing for prepare and commit

--- packages/payments-adapter-icash-pay/__tests__/icash-pay-payment.spec.ts
+++ packages/payments-adapter-icash-pay/__tests__/icash-pay-payment.spec.ts
@@ -1,7 +1,8 @@
-import { ICashPayBaseUrls, ICashPayPaymentInitOptions, LogLevel } from '../src/typing';
+import { ICashPayBaseUrls, ICashPayPaymentInitOptions, ICashPayPrepareOptions, LogLevel } from '../src/typing';
 import { ICashPayPayment } from '../src/icash-pay-payment';
 import debug from 'debug';
 import { iCashPayDebug, iCashPayDebugInfo } from '../src/debug';
+import { ICashPayOrder } from '../src/icash-pay-order';
 
 jest.mock('../src/debug', () => ({
   iCashPayDebug: jest.fn(),
@@ -11,22 +12,52 @@ jest.mock('../src/debug', () => ({
 
 jest.mock('../src/icash-pay-order');
 
-describe('iCash Pay Payment', () => {
+describe('ICashPay Payment', () => {
   let payment: ICashPayPayment;
 
-  const mockKey = '12345678901234567890123456789012'; // 32 bytes
-  const mockIv = '1234567890abcdef'; // 16 bytes
+  const mockAESKey = '12345678901234567890123456789012'; // 32 bytes
+  const mockAESIv = '1234567890abcdef'; // 16 bytes
+
+  const MockOrder = ICashPayOrder as jest.MockedClass<typeof ICashPayOrder>;
 
   const mockOptions: ICashPayPaymentInitOptions = {
     baseUrl: 'https://example.com' as ICashPayBaseUrls,
     merchantId: 'TEST_MERCHANT_1',
-    clientPrivateKey: 'TEST_PRIVATEKEY',
-    serverPublicKey: 'TEST_PUBLICKEY',
+    clientPrivateKey: 'TEST_CLIENT_PRIVATE_KEY',
+    serverPublicKey: 'TEST_SERVER_PUBLIC_KEY',
     aesKey: {
       id: 'TEST_AES_KEY_ID',
-      key: mockKey,
-      iv: mockIv,
+      key: mockAESKey,
+      iv: mockAESIv,
     },
+  };
+
+  const mockItems = [
+    {
+      name: 'TEST_ITEM_NAME_1',
+      unitPrice: 100,
+      quantity: 10,
+    },
+    {
+      name: 'TEST_ITEM_NAME_2',
+      unitPrice: 200,
+      quantity: 5,
+    },
+  ];
+
+  const mockPayPrepareOptions: ICashPayPrepareOptions = {
+    id: 'TEST_ORDER_ID',
+    items: mockItems,
+    storeId: 'TEST_STORE_ID',
+    storeName: 'TEST_STORE_NAME',
+    barcode: 'TEST_BARCODE',
+    amount: 1000,
+    collectedAmount: 800,
+    consignmentAmount: 200,
+    nonRedeemAmount: 100,
+    collectedNonRedeemAmount: 80,
+    consignmentNonRedeemAmount: 20,
+    nonPointAmount: 50,
   };
 
   describe('constructor', () => {
@@ -58,6 +89,121 @@ describe('iCash Pay Payment', () => {
       );
 
       expect(iCashPayDebugInfo).toHaveBeenCalledWith('Initialized iCashPay Payment Gateway');
+    });
+  });
+
+  describe('Functions', () => {
+    beforeEach(() => {
+      payment = new ICashPayPayment(mockOptions);
+    });
+
+    describe('ICashPayPayment.prepare', () => {
+      it('should throw error if amount does not match sum of item prices', async () => {
+        await expect(
+          payment.prepare({
+            ...mockPayPrepareOptions,
+            amount: 500,
+          }),
+        ).rejects.toThrow('Total amount does not match the sum of item prices');
+      });
+
+      it('should throw error if amount is zero', async () => {
+        await expect(
+          payment.prepare({
+            ...mockPayPrepareOptions,
+            items: [
+              {
+                name: 'TEST_ITEM_NAME_1',
+                unitPrice: 100,
+                quantity: 0,
+              },
+              {
+                name: 'TEST_ITEM_NAME_2',
+                unitPrice: 200,
+                quantity: 0,
+              },
+            ],
+            amount: 0,
+            consignmentAmount: 0,
+            collectedAmount: 0,
+          }),
+        ).rejects.toThrow('Total amount must be greater than 0');
+      });
+
+      describe('success', () => {
+        beforeEach(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should return new order - with id', async () => {
+          await payment.prepare(mockPayPrepareOptions);
+
+          expect(MockOrder).toHaveBeenCalledTimes(1);
+
+          expect(MockOrder).toHaveBeenCalledWith(
+            expect.objectContaining({
+              id: 'TEST_ORDER_ID',
+              items: mockItems,
+              gateway: payment,
+              createdAt: expect.any(Date),
+              deductEncData: expect.any(String),
+              isTWQRCode: false,
+              isRefunded: false,
+              paidAmount: 2000,
+              bonusAmount: 0,
+            }),
+          );
+        });
+
+        it('should return new order - with no id', async () => {
+          await payment.prepare({ ...mockPayPrepareOptions, id: undefined });
+
+          expect(MockOrder).toHaveBeenCalledTimes(1);
+
+          expect(MockOrder).toHaveBeenCalledWith(
+            expect.objectContaining({
+              id: expect.any(String),
+              items: mockItems,
+              gateway: payment,
+              createdAt: expect.any(Date),
+              deductEncData: expect.any(String),
+              isTWQRCode: false,
+              isRefunded: false,
+              paidAmount: 2000,
+              bonusAmount: 0,
+            }),
+          );
+        });
+
+        it('should return new order - no collected amount and consigment amount', async () => {
+          await payment.prepare({
+            ...mockPayPrepareOptions,
+            amount: 2000,
+            collectedAmount: undefined,
+            consignmentAmount: undefined,
+            collectedNonRedeemAmount: undefined,
+            consignmentNonRedeemAmount: undefined,
+            nonRedeemAmount: undefined,
+            nonPointAmount: undefined,
+          });
+
+          expect(MockOrder).toHaveBeenCalledTimes(1);
+
+          expect(MockOrder).toHaveBeenCalledWith(
+            expect.objectContaining({
+              id: expect.any(String),
+              items: mockItems,
+              gateway: payment,
+              createdAt: expect.any(Date),
+              deductEncData: expect.any(String),
+              isTWQRCode: false,
+              isRefunded: false,
+              paidAmount: 2000,
+              bonusAmount: 0,
+            }),
+          );
+        });
+      });
     });
   });
 });
---
+++ packages/payments-adapter-icash-pay/__tests__/icash-pay-payments-axios.spec.ts
@@ -0,0 +1,112 @@
+import {
+  I_CASH_PAY_SUCCESS_CODE,
+  ICashPayBaseUrls,
+  ICashPayPaymentInitOptions,
+  ICashPayResponse,
+  ICashPayTradeStatus,
+} from '../src/typing';
+import { ICashPayPayment } from '../src/icash-pay-payment';
+import axios from 'axios';
+import * as crypto from 'node:crypto';
+import { ICashPayOrder } from '../src/icash-pay-order';
+
+jest.mock('node:crypto');
+
+const fakePayload = {
+  TradeStatus: ICashPayTradeStatus.COMMITTED,
+  TotalAmount: '2000',
+  ICPAmount: '1500',
+  BonusAmt: '500',
+  PaymentDate: '2025/09/30 12:34:56',
+  TransactionID: 'TXN123456',
+  ICPAccount: 'ICP-ACC-001',
+  PaymentType: 'CREDIT_CARD',
+  MMemberID: 'MEMBER-999',
+  MobileInvoiceCarry: 'MOBILE-CARRIER',
+  MaskedPan: '123456******7890',
+  GID: 'UNIGID-001',
+};
+
+jest.mock('node:crypto', () => {
+  const actual = jest.requireActual('node:crypto');
+  const fakeDecipher = {
+    update: jest.fn(() => Buffer.from(JSON.stringify(fakePayload))),
+    final: jest.fn(() => Buffer.alloc(0)),
+  };
+
+  return {
+    ...actual,
+    createDecipheriv: jest.fn(() => fakeDecipher),
+    sign: jest.fn(() => Buffer.from('MOCK_SIGNATURE')),
+    verify: jest.fn(() => true),
+  };
+});
+
+jest.mock('axios');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+jest.mock('../src/icash-pay-order');
+
+describe('with mocked axios, crypto', () => {
+  let payment: ICashPayPayment;
+
+  const mockAESKey = '12345678901234567890123456789012'; // 32 bytes
+  const mockAESIv = '1234567890abcdef'; // 16 bytes
+
+  const MockOrder = ICashPayOrder as jest.MockedClass<typeof ICashPayOrder>;
+
+  const mockOptions: ICashPayPaymentInitOptions = {
+    baseUrl: 'https://example.com' as ICashPayBaseUrls,
+    merchantId: 'TEST_MERCHANT_1',
+    clientPrivateKey: 'TEST_CLIENT_PRIVATE_KEY',
+    serverPublicKey: 'TEST_SERVER_PUBLIC_KEY',
+    aesKey: {
+      id: 'TEST_AES_KEY_ID',
+      key: mockAESKey,
+      iv: mockAESIv,
+    },
+  };
+
+  const mockEncData = 'TEST_ENCDATA';
+  const mockAxiosSuccessResponse = {
+    data: { EncData: mockEncData, RtnCode: I_CASH_PAY_SUCCESS_CODE, RtnMsg: 'Success' } as ICashPayResponse,
+    status: 200,
+    statusText: 'OK',
+    headers: { 'x-req-id': 'abc' },
+    config: {},
+  };
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    payment = new ICashPayPayment(mockOptions);
+
+    (crypto.sign as jest.Mock).mockReturnValueOnce(Buffer.from('MOCK_SIGNATURE'));
+    (crypto.verify as jest.Mock).mockReturnValue(true);
+    mockedAxios.post.mockResolvedValue(mockAxiosSuccessResponse);
+  });
+
+  describe('ICashPayPayment.commit', () => {
+    it('should throw error if RtnCode is not a success code', async () => {
+      mockedAxios.post.mockResolvedValueOnce({
+        ...mockAxiosSuccessResponse,
+        data: { ...mockAxiosSuccessResponse.data, RtnCode: '9999', RtnMsg: 'Payment failed' },
+      });
+
+      await expect(payment.commit(mockEncData)).rejects.toThrow('[9999] Payment failed');
+    });
+
+    it('should throw error if is not verification failed', async () => {
+      (crypto.verify as jest.Mock).mockReturnValue(false);
+
+      await expect(payment.commit(mockEncData)).rejects.toThrow('[-999] Signature verification failed');
+    });
+
+    it('should return responsePayload - success', async () => {
+      const res = await payment.commit(mockEncData);
+
+      expect(res).toMatchObject(fakePayload);
+    });
+  });
+});
