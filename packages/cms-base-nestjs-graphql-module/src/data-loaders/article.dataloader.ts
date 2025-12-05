f57f5ab77d94cbc1bb0cf3bb5b79e5194cda33c2
Luca <lucaluca.tw@gmail.com>
2025-12-05 17:09:14 +0800

    fix(cms-base-nestjs-graphql-module): use categoriesLoaderNoCache in BackstageArticleResolver for uncached category loading

--- packages/cms-base-nestjs-graphql-module/src/data-loaders/article.dataloader.ts
+++ packages/cms-base-nestjs-graphql-module/src/data-loaders/article.dataloader.ts
@@ -12,6 +12,48 @@ export class ArticleDataLoader {
     private readonly articleRepo: Repository<BaseArticleEntity>,
   ) {}
 
+  private readonly batchLoadCategories = async (
+    queryArgs: readonly {
+      articleId: string;
+      language?: string;
+    }[],
+  ): Promise<CategoryDto[][]> => {
+    const qb = this.articleRepo.createQueryBuilder('articles');
+
+    qb.leftJoinAndSelect('articles.categories', 'categories');
+    qb.leftJoinAndSelect('categories.multiLanguageNames', 'multiLanguageNames');
+
+    qb.andWhere('articles.id IN (:...ids)', {
+      ids: queryArgs.map(arg => arg.articleId),
+    });
+
+    const articles = await qb.getMany();
+
+    const categoryMap = articles.reduce<Record<string, CategoryDto[]>>(
+      (vars, article) =>
+        article.categories.reduce(
+          (cVars, category) =>
+            category.multiLanguageNames.reduce(
+              (mVars, multiLanguageName) => ({
+                ...mVars,
+                [`${article.id}:${multiLanguageName.language}`]: [
+                  ...(mVars[`${article.id}:${multiLanguageName.language}`] || []),
+                  {
+                    ...category,
+                    ...multiLanguageName,
+                  },
+                ],
+              }),
+              cVars,
+            ),
+          vars,
+        ),
+      {},
+    );
+
+    return queryArgs.map(arg => categoryMap[`${arg.articleId}:${arg.language ?? DEFAULT_LANGUAGE}`] ?? []);
+  };
+
   readonly categoriesLoader = new DataLoader<
     {
       articleId: string;
@@ -19,56 +61,24 @@ export class ArticleDataLoader {
     },
     CategoryDto[],
     string
-  >(
-    async (
-      queryArgs: readonly {
-        articleId: string;
-        language?: string;
-      }[],
-    ): Promise<CategoryDto[][]> => {
-      const qb = this.articleRepo.createQueryBuilder('articles');
+  >(this.batchLoadCategories, {
+    cache: true,
+    cacheMap: new LRUCache<string, Promise<CategoryDto[]>>({
+      ttl: 1000 * 60, // 1 minute
+      ttlAutopurge: true,
+      max: 1000,
+    }),
+    cacheKeyFn: (queryArgs): string => `${queryArgs.articleId}:${queryArgs.language ?? DEFAULT_LANGUAGE}`,
+  });
 
-      qb.leftJoinAndSelect('articles.categories', 'categories');
-      qb.leftJoinAndSelect('categories.multiLanguageNames', 'multiLanguageNames');
-
-      qb.andWhere('articles.id IN (:...ids)', {
-        ids: queryArgs.map(arg => arg.articleId),
-      });
-
-      const articles = await qb.getMany();
-
-      const categoryMap = articles.reduce<Record<string, CategoryDto[]>>(
-        (vars, article) =>
-          article.categories.reduce(
-            (cVars, category) =>
-              category.multiLanguageNames.reduce(
-                (mVars, multiLanguageName) => ({
-                  ...mVars,
-                  [`${article.id}:${multiLanguageName.language}`]: [
-                    ...(mVars[`${article.id}:${multiLanguageName.language}`] || []),
-                    {
-                      ...category,
-                      ...multiLanguageName,
-                    },
-                  ],
-                }),
-                cVars,
-              ),
-            vars,
-          ),
-        {},
-      );
-
-      return queryArgs.map(arg => categoryMap[`${arg.articleId}:${arg.language ?? DEFAULT_LANGUAGE}`] ?? []);
-    },
+  readonly categoriesLoaderNoCache = new DataLoader<
     {
-      cache: true,
-      cacheMap: new LRUCache<string, Promise<CategoryDto[]>>({
-        ttl: 1000 * 60, // 1 minute
-        ttlAutopurge: true,
-        max: 1000,
-      }),
-      cacheKeyFn: (queryArgs): string => `${queryArgs.articleId}:${queryArgs.language ?? DEFAULT_LANGUAGE}`,
+      articleId: string;
+      language?: string;
     },
-  );
+    CategoryDto[],
+    string
+  >(this.batchLoadCategories, {
+    cache: false,
+  });
 }
