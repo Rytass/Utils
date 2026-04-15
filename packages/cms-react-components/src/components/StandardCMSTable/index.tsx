f191e21c34b1b22faf27f1db3a6a9003763ad983
TingTing <ting@rytass.com>
2026-04-15 18:32:10 +0800

    fix(packages/cms-react-components): components props

--- packages/cms-react-components/src/components/StandardCMSTable/index.tsx
+++ packages/cms-react-components/src/components/StandardCMSTable/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { TableDataSourceWithID } from '@mezzanine-ui/core/table';
+import { TableDataSourceWithId } from '@mezzanine-ui/core/table';
 import { CalendarConfigProvider } from '@mezzanine-ui/react';
 import calendarMethodsDayjs from '@mezzanine-ui/core/calendarMethodsDayjs';
 import DialogProvider from '../dialog/DialogProvider';
@@ -7,7 +7,7 @@ import ModalProvider from '../modal/ModalProvider';
 import Table from './Table';
 import { StandardCMSTableProps } from './typings';
 
-const StandardCMSTable = <T extends TableDataSourceWithID>(props: StandardCMSTableProps<T>): ReactElement => {
+const StandardCMSTable = <T extends TableDataSourceWithId>(props: StandardCMSTableProps<T>): ReactElement => {
   return (
     <CalendarConfigProvider methods={calendarMethodsDayjs}>
       <DialogProvider>
